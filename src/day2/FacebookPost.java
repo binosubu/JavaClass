96f3770dace9639109054f7ad3d050818164b027
Binod Maharjan <binosubu@gmail.com>
2022-03-22 10:11:18 -0600

    adding day2, fundamentals of OOP

---
+++ src/day2/FacebookPost.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class FacebookPost {
+
+    //States
+    //user
+    //taggedUser
+    //comment
+    //howManyLikes
+    //howManyComments
+    //howManyShares
+
+    //Behaviour
+    //expandThePost
+    //closeThePost
+    //commentTime
+    //viewMoreComments
+    //viewPreviousComments
+    //addingNewComments
+
+
+}
