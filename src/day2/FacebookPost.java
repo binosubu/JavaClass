5dd5481306d0593a56c36493377ece9bc863812b
Binod Maharjan <binosubu@gmail.com>
2022-03-23 09:44:00 -0600

    added primitive data type

--- src/day2/FacebookPost.java
+++ src/day2/FacebookPost.java
@@ -4,13 +4,25 @@ public class FacebookPost {
 
     //States(variables)
     //user
+    String user;
     //taggedUser
+    String taggedUser;
     //comment
+    String comment;
     //images
+    String images;
     //videos
+    String videos;
     //numberOfLikes
+    /*
+    confusion in case of more than 1000 likes it will be like 1.2K likes,
+    so will it be float or int
+     */
+    int numberOfLikes;
     //numberOfComments
+    int numberOfComments;
     //numberOfShares
+    int numberOfShares;
 
     //Behaviour (Methods and functions)
     //expandThePost
@@ -19,7 +31,5 @@ public class FacebookPost {
     //viewMoreComments
     //viewPreviousComments
     //addingNewComments
-
-
 
 }
