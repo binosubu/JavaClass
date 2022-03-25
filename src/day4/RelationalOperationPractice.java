25e8bf4d646516fe98f3bff050dda4b4f3eb91a0
Binod Maharjan <binosubu@gmail.com>
2022-03-25 11:14:46 -0600

    adding day4 files Relational Operation pratice

---
+++ src/day4/RelationalOperationPractice.java
@@ -0,0 +1,27 @@
+package day4;
+
+public class RelationalOperationPractice {
+    public static void main(String[]args){
+        int m=2;
+        int n=3;
+
+        //boolean isEqual = m!=n;
+        //System.out.println(isEqual);
+
+        //boolean isLessThan = m > n;
+        //System.out.println(isLessThan);
+
+        //boolean isLessThanEqualTo = m >= n;
+        //System.out.println(isLessThanEqualTo);
+
+        // && meaning both must be true;
+
+        //boolean and = (m<n) && (m==n);
+        //System.out.println(and);
+
+        int age = 22;
+        String message = (age >= 21) ? "You are allowed" : " You are not allowed";
+        System.out.println(message);
+
+    }
+}
