74f76f36b4b59de35c0128990718381ed1b5af04
Binod Maharjan <binosubu@gmail.com>
2022-03-28 21:57:40 -0600

    adding data type practice on day3 module

---
+++ src/day3/DataTypePractice.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class DataTypePractice {
+    // static int a;
+    // static byte a;
+    // static short a;
+    // static long a;
+    // static float a;
+    // static double a;
+    // for numeric datatype the default value is o
+    // static boolean a; for boolean default value is false
+    // static char a; for alpha numeric default value is null
+
+
+    public static void main(String[] args) {
+        System.out.println(a);
+
+    }
+}
